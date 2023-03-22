9fd5b94d5b2d3073b8d6190f5bd33d91df061aa8
shojol021 <shojol021@gmail.com>
2023-03-22 22:20:14 +0600

    usestate practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Increase color= 'Orange'></Increase>
+      <Increase color= 'White'></Increase>
+      <Increase color= 'Black'></Increase>
     </div>
   );
+}
+
+function Increase(props) {
+  const [count, setCount] = useState(1);
+
+  const boost = () => {setCount (count * 2)}
+
+  return (
+    <div style={{backgroundColor: 'orange', padding: '10px', margin: '20px'}}>
+      <h1>Color: {props.color}</h1>
+      <p>Click Here</p>
+      <p>Count: {count}</p>
+      <button onClick={boost}>Double</button>
+    </div>
+  )
 }
 
 export default App;
